d38d6ce8fcc8ed54bfa06fcbba57e21078a2b746
HassanAlex <Hassan.elsayed.ammer@gmail.com>
2022-11-14 02:31:21 +0200

    Set up Shoe Store App By hassan Elsayed Mohamed

---
+++ app/src/main/java/com/hassan/elsayed/ammer/shoe_store/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hassan.elsayed.ammer.shoe_store.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hassan.elsayed.ammer.shoe_store.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/hassan/elsayed/ammer/shoe_store/ui/MainActivityViewModel.kt
@@ -0,0 +1,7 @@
+package com.hassan.elsayed.ammer.shoe_store.ui
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel: ViewModel() {
+
+}
